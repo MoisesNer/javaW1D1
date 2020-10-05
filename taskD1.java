8c489340e522146297fcc678dc2194862e52e339
Moises Nervis <Nervis@Macs-iMac-2.local>
2020-10-05 10:45:48 -0700

    first commit

---
+++ taskD1.java
@@ -0,0 +1,12 @@
+public class taskD1 {
+    public static void main(String[] args){
+
+        int birthday = 20180110;
+        System.out.println(birthday);
+
+        birthday = 20180129;
+        System.out.println(birthday);
+    
+
+    }
+}
